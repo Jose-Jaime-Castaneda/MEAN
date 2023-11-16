e25b039e55a3fcb17cc10d8cc1818be8b65607d2
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-11-16 15:02:39 -0600

    Terminamos configuración del FrontEnd

---
+++ src/app/components/listar-producto/listar-producto.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-listar-producto',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './listar-producto.component.html',
+  styleUrl: './listar-producto.component.css'
+})
+export class ListarProductoComponent {
+
+}
---
+++ src/app/models/modelo.ts
@@ -0,0 +1,12 @@
+export class Producto {
+    _id?: number;
+    nombre: string;
+    categoria: string;
+    precio: number;
+
+    constructor(nombre: string, categoria: string, precio: number){
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+}
