a7fc6477f996c613f6950fdf7f1e0828b3cc9bd9
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-11-17 15:29:00 -0600

    Control de formulario crearProducto listo

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-producto',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './crear-producto.component.html',
   styleUrl: './crear-producto.component.css'
 })
 export class CrearProductoComponent {
+  productoForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.productoForm = this.fb.group({
+      producto: ['', Validators.required],
+      categoria: ['', Validators.required],
+      precio: ['', Validators.required],
+    })
+  }
+
+  agregarProdcuto() {
+    console.log(this.productoForm);
+  }
 }
