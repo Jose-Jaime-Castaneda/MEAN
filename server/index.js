6e542ec3ead3ce7657a2ad31273b598d33b56d4c
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-11-17 16:05:30 -0600

    Creamos la primer API de prueba

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log('desde index.js');
+const express = require('express');
+
+const app = express();
+
+app.get('/',  (req, res) =>{ 
+    res.send('Hola mundo');
+});
+
+app.listen(4000, () => {
+    console.log('El servidor funciona');
+})
