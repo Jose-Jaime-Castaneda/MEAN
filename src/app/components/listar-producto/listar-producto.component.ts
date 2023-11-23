c503b18501b4eacbbec9138f2ffe4513ca35818f
JoseCastañeda <77752153+josejaimrgcr@users.noreply.github.com>
2023-11-23 17:02:41 -0600

    Configuración del HttpClient y primer get

--- src/app/components/listar-producto/listar-producto.component.ts
+++ src/app/components/listar-producto/listar-producto.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { ProductoService } from '../../services/producto.service';
 
 @Component({
   selector: 'app-listar-producto',
@@ -11,4 +12,20 @@ import { RouterLink } from '@angular/router';
 })
 export class ListarProductoComponent {
 
+  productos: any[] = [];
+
+  constructor(private productoService: ProductoService) { }
+
+  ngOnInit(): void {
+    this.obtenerProductos();
+    console.log(this.productos);
+  }
+
+  obtenerProductos() {
+    this.productoService.getProductos().subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+    })
+  }
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+  private url: string;
+
+  constructor(private http: HttpClient,) {
+    this.url = 'http://localhost:4000/api/productos/';
+  }
+
+  getProductos(): Observable<any> {
+    return this.http.get(this.url);
+  }
 }
